18e009e2c1000facac15797609a25f2c52c0d81e
Sanjay Khandla <sanjay.khandla@solutionanalysts.in>
2023-10-13 16:19:04 +0530

    add

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,30 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function ImageUpload() {
+    const [image, setImage] = useState<any>('');
 
-    const [image, setImaje] = useState(null)
-    const uploadImage = async () => {
-        const formData: any = new FormData()
-        formData.append("file", image)
-        try {
-            const response = await fetch("http://localhost:3003/api/auth/register", { method: "post", body: formData })
-            if (response) {
-                console.log("image is uploaded")
-            } else {
-                console.log("not uploaded")
-            }
-        } catch (error) {
-            console.log("error ============== ", error)
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            console.log("New image selected:", e.target.files[0]);
+            setImage(e.target.files[0]);
         }
-    }
+    };  
 
+    const uploadImage = async () => {
+        if (image) {
+            const formData = new FormData();
+            formData.append("file", image);
+
+            try {
+                const response = await fetch("http://localhost:3003/api/auth/register", {
+                    method: "POST",
+                    body: formData,
+                });
+
+                if (response.status === 200) {
+                    console.log("Image is uploaded");
+                    setImage(''); // Clear the image state after successful upload
+                    const imageInput: any = document.getElementById('imageInput');
+                    if (imageInput) {
+                        imageInput.value = ''; // Clear the file input
+                    }
+                } else {
+                    console.log("Image not uploaded");
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        } else {
+            console.log("No image selected to upload");
+        }
+    };
 
     return (
         <div>
-            <input type="file" onChange={(e: any) => {
-                setImaje(e.target.files[0])
-            }}></input>
-            <button onClick={uploadImage}>upload</button>
+            <input type="file" id="imageInput" onChange={handleImageChange} />
+            <button onClick={uploadImage}>Upload</button>
         </div>
     );
 }
